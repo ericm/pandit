476bba90dc55c68599d50d097e82c0b984828f77
Eric Moynihan <git@moynihan.io>
2021-11-02 09:44:49 +0000

    payload?

--- src/bpf/xdp_parser.c
+++ src/bpf/xdp_parser.c
@@ -19,26 +19,30 @@ struct {
     char *value;
 } lookups SEC(".maps");
 
-static void parse_header(char *data, const char *data_end, size_t offset, void *hdr) {
+static void parse_header(char *data, const char *data_end, size_t offset, char ***hdr) {
     if (data+offset > data_end)
         return;
 
     char *l_hdr = data+offset;
-    hdr = l_hdr;
+    **hdr = l_hdr;
+    bpf_printk("HDR = %p", hdr);
 }
 
 static int from_data(struct xdp_md *ctx, char **payload, const u32 *ip, char **data, char **data_end) {
     struct iphdr *ip_hdr;
     char *l_data, *l_data_end;
 
+    if (*data<*data_end)
+        return -1;
+
     l_data = (char *)(long) ctx->data;
     l_data_end = (char *)(long)ctx->data_end;
     data = &l_data;
     data_end = &l_data_end;
 
-    parse_header(l_data, l_data_end, sizeof(struct ethhdr), &ip_hdr);
+//    parse_header(l_data, l_data_end, sizeof(struct ethhdr), &ip_hdr);
     parse_header(l_data, l_data_end, sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct tcphdr), &payload);
-    ip = &ip_hdr->daddr;
+//    ip = &ip_hdr->daddr;
 
     return -1;
 }
@@ -62,6 +66,7 @@ SEC("xdp")
 int handle_egress_packet(struct xdp_md *ctx) {
     bpf_printk("Packet received");
     char *data, *data_end, *payload;
+//    https://github.com/xdp-project/xdp-tutorial/blob/master/packet02-rewriting/xdp_prog_kern.c
     u32 ip;
     int ret;
 
@@ -69,8 +74,8 @@ int handle_egress_packet(struct xdp_md *ctx) {
     if (ret > -1)
         return ret;
 
-    bpf_printk("Payload len = %p", (uintptr_t)payload);
-    if (data_end-payload < 8)
+    bpf_printk("Payload = %p", payload);
+    if (data_end-payload < sizeof(HTTP))
         return XDP_PASS;
     ret = parse_payload(&ip, data, payload);
     if (ret > -1)
