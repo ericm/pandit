0a863a43149fd91dd9b2f1ec3466c13ecb0217cb
Eric Moynihan <git@moynihan.io>
2021-11-02 15:41:23 +0000

    Add libxdp

--- src/bpf/xdp_parser.c
+++ src/bpf/xdp_parser.c
@@ -3,6 +3,7 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_tracing.h>
+#include <xdp/xdp_helpers.h>
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
@@ -20,53 +21,32 @@ struct {
     char *value;
 } lookups SEC(".maps");
 
-static int parse_header(char *data, const char *data_end, size_t offset, char ***hdr) {
-    if (data+offset >= data_end)
-        return XDP_PASS;
+/*
+ * parse_tcppld: parse and return the length of the tcp payload
+ */
+static __always_inline int parse_tcppld(struct hdr_cursor *cursor,
+                                        void *data_end,
+                                        char **tcppld)
+{
+    int len;
+    char *h = cursor->pos;
 
-    char *l_hdr = data+offset;
-    **hdr = l_hdr;
-    bpf_printk("HDR = %p", hdr);
-    return -1;
-}
-
-static int from_data(struct xdp_md *ctx, char **payload, const u32 *ip, char **data, char **data_end) {
-    struct iphdr *ip_hdr;
-    char *l_data, *l_data_end;
-    int ret;
-
-    if (*data<*data_end)
+    if ((void *)(h + 1) > data_end)
         return -1;
 
-    l_data = (char *)(long) ctx->data;
-    l_data_end = (char *)(long)ctx->data_end;
-    *data = l_data;
-    *data_end = l_data_end;
+    len = (int)((unsigned long long)data_end - (unsigned long long)h);
+    /* Sanity check packet field is valid */
+    if(len < sizeof(*h))
+        return -1;
 
-//    parse_header(l_data, l_data_end, sizeof(struct ethhdr), &ip_hdr);
-    ret = parse_header(l_data, l_data_end, sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct tcphdr), &payload);
-    if (ret > -1) {
-        return ret;
-    }
-//    ip = &ip_hdr->daddr;
+    /* Variable-length TCP header, need to use byte-based arithmetic */
+    if (cursor->pos + len > data_end)
+        return -1;
 
-    return -1;
-}
+    cursor->pos += len;
+    *tcppld = h;
 
-static int parse_payload(u32 *ip, char *data, char *payload) {
-    struct lookup *lookup = bpf_map_lookup_elem(&lookups, &ip);
-
-    if (!lookup && payload) {
-        for (int i = 0; i < sizeof(HTTP); i++) {
-            if (*(payload+i) != HTTP[i]) {
-                bpf_printk("Error parsing %s", *payload);
-                return XDP_PASS;
-            }
-        }
-        bpf_printk("Packet passed: %s", payload);
-        bpf_map_update_elem(&lookups, &ip, &payload, BPF_ANY);
-    }
-    return -1;
+    return len;
 }
 
 SEC("xdp")
@@ -76,7 +56,7 @@ int handle_egress_packet(struct xdp_md *ctx) {
     void *data_end = (void *)(unsigned long long)ctx->data_end;
     void *data = (void *)(unsigned long long)ctx->data;
 
-    struct hdr_cursor nh;
+    struct hdr_cursor cursor;
     struct ethhdr *eth;
     int eth_type;
     int ip_type;
@@ -84,24 +64,25 @@ int handle_egress_packet(struct xdp_md *ctx) {
     struct iphdr *iphdr;
     struct ipv6hdr *ipv6hdr;
     struct tcphdr *tcphdr;
+    char *tcppld;
 
-    nh.pos = data;
+    cursor.pos = data;
 
-    eth_type = parse_ethhdr(&nh, data_end, &eth);
+    eth_type = parse_ethhdr(&cursor, data_end, &eth);
     if (eth_type == bpf_htons(ETH_P_IP)) {
-        ip_type = parse_iphdr(&nh, data_end, &iphdr);
+        ip_type = parse_iphdr(&cursor, data_end, &iphdr);
         if (ip_type != IPPROTO_TCP)
             return XDP_PASS;
     }
     else if (eth_type == bpf_htons(ETH_P_IPV6)) {
-        ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
+        ip_type = parse_ip6hdr(&cursor, data_end, &ipv6hdr);
         if (ip_type != IPPROTO_TCP)
             return XDP_PASS;
     } else {
         return XDP_PASS;
     }
 
-    tcp_type = parse_tcphdr(&nh, data_end, &tcphdr);
+    tcp_type = parse_tcphdr(&cursor, data_end, &tcphdr);
     if ((void *)(tcphdr + 1) > data_end) {
         return XDP_PASS;
     }
@@ -109,7 +90,14 @@ int handle_egress_packet(struct xdp_md *ctx) {
     switch (tcphdr->dest) {
         case bpf_htons(8000):
             bpf_printk("Right Port");
+            break;
+        default:
+            return XDP_PASS;
     }
+
+    parse_tcppld(&cursor, data_end, &tcppld);
+
+    bpf_printk("Packet: %s", tcppld);
 
     return XDP_PASS;
 }
