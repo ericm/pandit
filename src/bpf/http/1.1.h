ea875cefef6ab3e60ba503ad9a90fa4a74e49aba
Eric Moynihan <git@moynihan.io>
2021-11-04 20:09:19 +0000

    backup

--- src/bpf/http/1.1.h
+++ src/bpf/http/1.1.h
@@ -1,4 +1,17 @@
 #include "../vmlinux.h"
 
-struct http1reqhdr {
+struct http1_1_req_hdr_t {
+    short version;
+};
+
+int parse_http1_1_req_hdr(struct http1_1_req_hdr_t *hdr, const char *buf, int len);{
+    int i = 0;
+    while (i < len) {
+        if (buf[i] == '\r' && buf[i + 1] == '\n') {
+            hdr->version = 1;
+            return i + 2;
+        }
+        i++;
+    }
+    return -1;
 };
