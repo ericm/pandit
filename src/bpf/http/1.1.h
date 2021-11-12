23aa3eaff331ce6f43262fbc97dbc64194c63194
Eric Moynihan <git@moynihan.io>
2021-11-12 12:18:43 +0000

    Fixed HTTP header parsing

--- src/bpf/http/1.1.h
+++ src/bpf/http/1.1.h
@@ -18,23 +18,25 @@ parse_http1_1_req_hdr(struct http1_1_req_hdr_t *hdr, const __u8 *buf, int len) {
     int lower, i;
     char *sb, *sb_sep;
 
-    if(__bpf_memcmp(&HTTP, h_buf, sizeof(HTTP)-1))
+
+    if(__bpf_memcmp(&HTTP, buf, sizeof(HTTP)-1))
         return 0;
 
-    __bpf_memcpy_builtin(&h_buf, h_buf+5, sizeof(h_buf));
+    __bpf_memcpy_builtin(&h_buf, buf+5, sizeof(h_buf));
+
     hdr->maj_version = h_buf[0]-ascii_offset;
     hdr->min_version = h_buf[2]-ascii_offset;
     bpf_printk("version: %d.%d", hdr->maj_version, hdr->min_version);
 
-    __bpf_memcpy_builtin(&h_buf, h_buf+9, sizeof(h_buf));
+    __bpf_memcpy_builtin(&h_buf, buf+9, sizeof(h_buf));
     hdr->code = (h_buf[0]*100 + h_buf[1]*10 + h_buf[2]) - ascii_offset - ascii_offset - ascii_offset;
 
     bpf_printk("len %d", len);
     
-    for (i = 13; i < len; i++) {
-        sb = strchr(sb, 13) + 1;
-        sb_sep = strchr(sb, ':');
-    }
+    // for (i = 13; i < len; i++) {
+    //     sb = strchr(sb, 13) + 1;
+    //     sb_sep = strchr(sb, ':');
+    // }
 
     return 1;
 };
