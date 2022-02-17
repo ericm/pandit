a5b89c341b006fbf560d88ee51dfb0d66a377779
Eric Moynihan <git@moynihan.io>
2022-02-17 19:36:53 +0000

    todo: i think i should change where i check cache options to probe instead of receive

--- src/broker/mod.rs
+++ src/broker/mod.rs
@@ -18,6 +18,26 @@ struct CachedMessage {
     fields: Fields,
 }
 
+impl CachedMessage {
+    pub fn set_fields_from_bytes(&mut self, buf: &[u8]) -> ServiceResult<()> {
+        let fields = self.message.fields_from_bytes(buf)?;
+        for (key, value) in fields.map {
+            let proto = self.message.fields_by_name.get(&key);
+            let proto = proto.ok_or(ServiceError::new(
+                format!("no field named: {}", key).as_str(),
+            ))?;
+            let proto = proto.value();
+            let cache = self
+                .cache
+                .as_ref()
+                .or(proto.cache.as_ref())
+                .unwrap_or_default();
+            if !cache.do_not_cache {}
+        }
+        Ok(())
+    }
+}
+
 pub struct Broker {
     cfg: config::Config,
     client: Client,
@@ -71,7 +91,7 @@ impl Broker {
                 name,
                 CachedMessage {
                     message: message.to_owned(),
-                    cache: method.cache,
+                    cache: method.cache.or(Some(service.default_cache)),
                     fields: Fields::new(Default::default()),
                 },
             );
@@ -181,7 +201,7 @@ impl Broker {
             Some(mut v) => {
                 let cached = v.value_mut();
                 let payload = msg.get_payload_bytes();
-                cached.fields = cached.message.fields_from_bytes(payload)?;
+                cached.set_fields_from_bytes(payload)?;
                 Ok(())
             }
             None => Err(ServiceError::new(
