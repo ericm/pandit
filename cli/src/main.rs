896199ebf63a075fd6eeb298d1f067dd9ed39799
Eric Moynihan <git@moynihan.io>
2022-03-11 14:15:49 +0000

    fix cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,7 @@
 use clap::{Parser, Subcommand};
 use config::Config;
 use grpcio::{ChannelBuilder, EnvBuilder};
+use serde::Deserialize;
 use std::{env::current_dir, fs::File, io::Read, path::PathBuf, sync::Arc};
 
 #[derive(Parser)]
@@ -20,6 +21,35 @@ enum ServiceCommand {
     Add { path: String },
 }
 
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+struct Panditfile {
+    metadata: Metadata,
+    docker: Option<Docker>,
+    k8s: Option<K8s>,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+struct Metadata {
+    name: String,
+    port: i32,
+    proto: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+struct Docker {
+    container_id: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+enum K8s {
+    Pod { name: String },
+    Service { name: String },
+}
+
 fn main() {
     let app: Args = Parser::parse();
     let env = Arc::new(EnvBuilder::new().build());
@@ -36,33 +66,47 @@ fn main() {
 
     match &app.service {
         ServiceCommand::Add { path } => {
-            let mut req = api_proto::api::StartServiceRequest::new();
             let path = PathBuf::from(path);
             let mut path = path.canonicalize().unwrap();
             if !path.ends_with(".toml") {
                 path = path.join("panditfile.toml");
             }
-            let cfg = Config::builder()
-                .add_source(config::File::with_name(path.to_str().unwrap()))
-                .build()
-                .unwrap();
-            let proto_name = cfg.get_string("service.proto").unwrap();
-            let mut proto_path = proto_path.join(proto_name.clone());
+            let cfg: Panditfile = {
+                let cfg = Config::builder()
+                    .add_source(config::File::with_name(path.to_str().unwrap()))
+                    .build()
+                    .unwrap();
+                cfg.try_deserialize().unwrap()
+            };
+
+            let mut proto_path = proto_path.join(&cfg.metadata.proto);
             proto_path.set_extension("proto");
 
-            let mut panditfile = File::open(&proto_path).unwrap();
             let mut proto = Vec::<u8>::new();
+            let mut panditfile = File::open(&proto_path).unwrap();
             panditfile.read_to_end(&mut proto).unwrap();
+
+            let mut req = api_proto::api::StartServiceRequest::new();
             req.set_proto(proto);
-            req.set_port(cfg.get_int("service.port").unwrap().try_into().unwrap());
-            req.set_container_id(cfg.get_string("service.container").unwrap_or_default());
-            let name = cfg.get_string("service.name").unwrap();
-            req.set_name(name.clone());
+            req.set_port(cfg.metadata.port);
+            req.set_name(cfg.metadata.name.clone());
+            match cfg.docker {
+                Some(docker) => {
+                    req.set_docker_id(docker.container_id);
+                }
+                None => match cfg.k8s {
+                    Some(k8s) => match k8s {
+                        K8s::Pod { name } => req.set_k8s_pod(name),
+                        K8s::Service { name } => req.set_k8s_service(name),
+                    },
+                    None => todo!(),
+                },
+            }
 
             let _resp = client.start_service(&req).unwrap();
             println!(
                 "Successfully created service {} with proto {}",
-                name, proto_name
+                cfg.metadata.name, cfg.metadata.proto
             );
         }
     }
